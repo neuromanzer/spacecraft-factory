2ea2b72c092e922345b1b83152c4a2f3094e943d
neuromanzer <krokusfs@yahoo.com>
2023-04-01 18:55:30 +0300

    Initial commit

---
+++ src/main/java/neuro/manzer/spacecraftfactory/SpacecraftFactoryApplication.java
@@ -0,0 +1,13 @@
+package neuro.manzer.spacecraftfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecraftFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacecraftFactoryApplication.class, args);
+	}
+
+}
